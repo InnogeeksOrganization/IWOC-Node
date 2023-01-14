7d4262dcdd8d918e4f2367070b728de3f9fb375c
“1010varun” <varunagarwal10.2@gmail.com>
2023-01-14 23:42:09 +0530

    updated: landing page

--- public/js/timer.js
+++ public/js/timer.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "02/01/",
+        dayMonth = "01/20/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
