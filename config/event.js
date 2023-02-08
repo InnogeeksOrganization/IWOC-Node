c01ee0bdc4545be07da7dee7e3991c839ab92a67
Ryanwalker277 <andianurag277@gmail.com>
2023-02-08 23:07:49 +0530

    Updated frontend for Event Registration page

--- config/event.js
+++ config/event.js
@@ -5,8 +5,9 @@ const eventRegistration = new mongoose.Schema({
     name: String,
     email : String,
     phone : String,
+    phone : String,
     libid: String,
-    eventName: {type: String, default:"example"}
+    eventName: {type: String, default:"11feb"}
 })
 
 const event = new mongoose.model('Event', eventRegistration);
