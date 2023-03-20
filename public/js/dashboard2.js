5289ff7cf8371ea8431516c16067e282baf0087e
Ryanwalker277 <andianurag277@gmail.com>
2023-03-21 00:59:55 +0530

    Minor Updates

--- public/js/dashboard2.js
+++ public/js/dashboard2.js
@@ -15,19 +15,3 @@ light.addEventListener("click", function () {
   light.classList.add("active");
   logo.setAttribute('src','../public/img/iwoc_logo_dark.png');
 });
-
-var certi_button = document.getElementById("certificate").addEventListener("click", click(user.name.split(' ')[0]))
-
-function click(user_name) {
-  var theForm, newInput1;
-  theForm = document.createElement('form');
-  theForm.action = '/certi';
-  theForm.method = 'POST';
-  newInput1 = document.createElement('input');
-  newInput1.type = 'hidden';
-  newInput1.name = 'name';
-  newInput1.value = user_name;
-  theForm.appendChild(newInput1);
-  document.getElementById('certificate').appendChild(theForm);
-  theForm.submit()
-}
